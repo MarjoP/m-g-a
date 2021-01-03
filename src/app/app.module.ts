1bddf6aeca926da36f846379047c6548d1aa31de
Marjo Pantsar <s2000589@edu.bc.fi>
2021-01-03 08:56:06 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CustomersModule } from './customers/customers.module';
+
+import { CoreModule} from './core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { OrdersModule } from './orders/orders.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule, 
+    CustomersModule,
+    OrdersModule,
+    AppRoutingModule,
+    SharedModule, 
+    CoreModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
